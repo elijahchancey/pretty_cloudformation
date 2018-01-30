a8eb913959b7665103be3613ab1374d7d58e983b
Peter Salnikov <peter@salnikov.cc>
2018-01-30 18:08:48 +0300

    #1 fixed

--- pretty_cloudformation.py
+++ pretty_cloudformation.py
@@ -27,13 +27,32 @@ class GenericScalar:
         return dumper.represent_scalar(
             data._tag, data._value, style=data._style)
 
+class GenericSequence:
+    def __init__(self, value, tag, flow_style=None):
+        self._value = list()
+        for item in value:
+            self._value.append(default_constructor(None, item.tag, item))
+        self._tag = tag
+        self._flow_style = flow_style
+
+    @staticmethod
+    def to_yaml(dumper, data):
+        # data is a GenericScalar
+        return dumper.represent_sequence(
+            data._tag, data._value, flow_style=data._flow_style)
 
 def default_constructor(loader, tag_suffix, node):
+    print('Loader:{}, tag_suffic:{}, node:{}'.format(loader, tag_suffix, node))
+
     if isinstance(node, yaml.ScalarNode):
         return GenericScalar(node.value, tag_suffix, style=node.style)
+    elif isinstance(node, yaml.SequenceNode):
+        # print(yaml.dump(node))
+        return GenericSequence(node.value, tag_suffix, flow_style=node.flow_style)
     else:
-        import json
-        print(node)
+        # import json
+        # print(node)
+        # print(node.__class__)
         raise NotImplementedError('Node: ' + str(type(node)))
 
 
@@ -41,6 +60,10 @@ yaml.add_multi_constructor('', default_constructor, Loader=yaml.SafeLoader)
 
 yaml.add_representer(
     GenericScalar, GenericScalar.to_yaml, Dumper=yaml.SafeDumper
+)
+
+yaml.add_representer(
+    GenericSequence, GenericSequence.to_yaml, Dumper=yaml.SafeDumper
 )
 
 for filename in args.filename:
